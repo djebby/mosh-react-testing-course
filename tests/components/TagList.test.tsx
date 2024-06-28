fec19e3b0bd8a2fa2a3904c685dddcc5b4f8abf3
djebby <djebby.firas@gmail.com>
2024-06-28 04:41:59 +0100

    testing asynchronous code

---
+++ tests/components/TagList.test.tsx
@@ -0,0 +1,19 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import TagList from '../../src/components/TagList';
+
+describe('TagList', () => {
+  it('should render tags', async () => {
+    render(<TagList />);
+
+    /* keep executing the funciton each 50ms until 1second is passed.
+    await waitFor(() => {
+      const listItems = screen.getAllByRole('listitem');
+      expect(listItems.length).toBeGreaterThan(0);
+    }); 
+    */
+
+    const listItems = await screen.findAllByRole("listitem");
+    expect(listItems.length).toBeGreaterThan(0);
+    
+  })
+});
