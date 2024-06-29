724e396d498b4284d1b29dbbc325239ac2f14e88
djebby <djebby.firas@gmail.com>
2024-06-29 18:58:57 +0100

    Testing OrderStatusSelector

--- tests/components/OrderStatusSelector.test.tsx
+++ tests/components/OrderStatusSelector.test.tsx
@@ -5,17 +5,22 @@ import { Theme } from '@radix-ui/themes';
 import OrderStatusSelector from '../../src/components/OrderStatusSelector';
 
 describe('OrderStatusSelector', () => {
+  
+  const onChange = vi.fn();
 
   const renderComponent = () => {
     render(
       <Theme>
-        <OrderStatusSelector onChange={vi.fn()} />
+        <OrderStatusSelector onChange={onChange} />
       </Theme>
     );
 
     return {
       trigger: screen.getByRole('combobox'),
       getOptions: () => screen.findAllByRole('option'),
+      getOption: (label: RegExp) => screen.findByRole('option', { name: label }),
+      user: userEvent.setup(),
+      onChange,
     };
   }
 
@@ -28,15 +33,40 @@ describe('OrderStatusSelector', () => {
   
   it('should render correct statuses', async () => {
     
-    const { trigger, getOptions } = renderComponent();
+    const {trigger, getOptions, user} = renderComponent();
 
-    const user = userEvent.setup();
     await user.click(trigger);
 
     const options = await getOptions();
     expect(options).toHaveLength(3);
     const labels = options.map(option => option.textContent);
     expect(labels).toEqual(['New', 'Processed', 'Fulfilled']);
+  });
+
+  it.each([
+    { label: /processed/i, value: 'processed' },
+    { label: /fulfilled/i, value: 'fulfilled' },
+  ])('should call onChange with $value when the $label option is selected', async ({ label, value }) => {
+    const { trigger, user, getOption, onChange } = renderComponent();
+    await user.click(trigger);
+    const option = await getOption(label);
+    await user.click(option);
+    expect(onChange).toHaveBeenCalledWith(value);
+
+  });
+
+  it(`should call onChange with 'new' when the /new/i option is selected`, async () => {
+    const { trigger, user, getOption, onChange } = renderComponent();
+    await user.click(trigger);
+    const processedOption = await getOption(/processed/i);
+    await user.click(processedOption);
+    
+    await user.click(trigger);
+    const newOption = await getOption(/new/i);
+    await user.click(newOption);
+
+    expect(onChange).toHaveBeenCalledWith('new');
+
   });
 
 });
