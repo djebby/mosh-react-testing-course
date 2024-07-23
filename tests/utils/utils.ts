db310edcc9775bed6cc438d3118c81bc748e3cb1
djebby <djebby.firas@gmail.com>
2024-07-23 11:45:10 +0100

    mocking the authentication status

--- tests/utils/utils.ts
+++ tests/utils/utils.ts
@@ -1,5 +1,6 @@
 import { http, HttpResponse, delay } from "msw";
 import { server } from '../mocks/server';
+import { useAuth0, User } from "@auth0/auth0-react";
 
 export const simulateDelay = (endpoint: string) => {
   server.use(http.get(endpoint, async () => {
@@ -14,3 +15,22 @@ export const simulateError = (endpoint: string) => {
     http.get(endpoint, () => HttpResponse.error()),
   );
 };
+
+type AuthState = {
+  user: User | undefined;
+  isAuthenticated: boolean;
+  isLoading: boolean;
+}
+
+export const mockAuthState = (authState: AuthState) => {
+  vi.mocked(useAuth0).mockReturnValue({
+    ...authState,
+    getAccessTokenSilently: vi.fn().mockResolvedValue('random-text'),
+    getAccessTokenWithPopup: vi.fn(),
+    getIdTokenClaims: vi.fn(),
+    loginWithRedirect: vi.fn(),
+    loginWithPopup: vi.fn(),
+    logout: vi.fn(),
+    handleRedirectCallback: vi.fn(),
+  });
+}
