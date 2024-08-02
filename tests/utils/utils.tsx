7bad43dd9e79a2ccc1e009c290a9a775bd0c2210
djebby <djebby.firas@gmail.com>
2024-08-02 20:37:46 +0100

    extracting navigateTo

--- tests/utils/utils.tsx
+++ tests/utils/utils.tsx
@@ -1,6 +1,9 @@
 import { http, HttpResponse, delay } from "msw";
 import { server } from '../mocks/server';
 import { useAuth0, User } from "@auth0/auth0-react";
+import { createMemoryRouter, RouterProvider } from "react-router-dom";
+import routes from "../../src/routes";
+import { render } from "@testing-library/react";
 
 export const simulateDelay = (endpoint: string) => {
   server.use(http.get(endpoint, async () => {
@@ -34,3 +37,10 @@ export const mockAuthState = (authState: AuthState) => {
     handleRedirectCallback: vi.fn(),
   });
 }
+
+export const navigateTo = (path: string) => {
+  const router = createMemoryRouter(routes, {
+    initialEntries: [path],
+  });
+  render(<RouterProvider router={router} />);
+};
